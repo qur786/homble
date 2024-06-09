21221f5aa361e0087daa4a568e970340a58363f3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 16:43:09 +0530

    refactor: update api calls with useFetcher

--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -2,54 +2,41 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { ProductCard } from "../components/ProductCard";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "../types";
-import { getRequest } from "../axios";
 import { Container } from "react-bootstrap";
 import { ProductModal } from "../components/ProductModal";
+import { useFetcher } from "../hooks/useFetcher";
 
 export function Products() {
-  /**
-   * @type {[Product[], import("react").Dispatch<Product[]>]}
-   */
-  const [products, setProducts] = useState([]);
   const [openModal, setOpenModal] = useState(false);
 
-  const showModal = () => {
-    setOpenModal(true);
-  };
+  const { makeRequest, output: products } = useFetcher("get");
 
-  const closeModal = () => {
+  const showModal = useCallback(() => {
+    setOpenModal(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
     setOpenModal(false);
-  };
+  }, []);
 
   useEffect(() => {
-    getRequest("/products")
-      .then((data) => {
-        /**
-         * @type {Product[]}
-         */
-        const output = data.data;
-        const sortedOutput = output.sort(
-          (a, b) => a.selling_price - b.selling_price,
-        );
-        setProducts(sortedOutput);
-      })
-      .catch(console.log);
-  }, []);
+    makeRequest({ url: "/products" });
+  }, [makeRequest]);
 
   return (
     <Container>
       <Row>
-        <Col sm={8} md={10} />
-        <Col sm={4} md={2}>
+        <Col xs={8} md={10} />
+        <Col xs={4} md={2}>
           <Button onClick={showModal} className="mt-4">
             Add Product
           </Button>
         </Col>
       </Row>
       <Row xs={1} md={2} lg={3} className="g-4 p-4">
-        {products.map((ele) => (
+        {products?.map((ele) => (
           <Col key={ele.id}>
             <ProductCard {...ele} />
           </Col>
