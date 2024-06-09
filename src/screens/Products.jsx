0d5a869b5a734056d546488b376d510f3703faff
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 13:43:11 +0530

    refactor: integrated ProductModal in Products page

--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -1,15 +1,27 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import { ProductCard } from "../components/ProductCard";
 import { useEffect, useState } from "react";
 import "../types";
 import { getRequest } from "../axios";
+import { Container } from "react-bootstrap";
+import { ProductModal } from "../components/ProductModal";
 
 export function Products() {
   /**
    * @type {[Product[], import("react").Dispatch<Product[]>]}
    */
   const [products, setProducts] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  const showModal = () => {
+    setOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
 
   useEffect(() => {
     getRequest("/products")
@@ -27,12 +39,27 @@ export function Products() {
   }, []);
 
   return (
-    <Row xs={1} md={2} lg={3} className="g-4 p-4">
-      {products.map((ele) => (
-        <Col key={ele.id}>
-          <ProductCard {...ele} />
+    <Container>
+      <Row>
+        <Col sm={8} md={10} />
+        <Col sm={4} md={2}>
+          <Button onClick={showModal} className="mt-4">
+            Add Product
+          </Button>
         </Col>
-      ))}
-    </Row>
+      </Row>
+      <Row xs={1} md={2} lg={3} className="g-4 p-4">
+        {products.map((ele) => (
+          <Col key={ele.id}>
+            <ProductCard {...ele} />
+          </Col>
+        ))}
+      </Row>
+      <ProductModal
+        open={openModal}
+        showModal={showModal}
+        closeModal={closeModal}
+      />
+    </Container>
   );
 }
