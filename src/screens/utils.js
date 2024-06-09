9c50035f05701255c39c986d6b9ec2b1ceb2de44
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 17:38:03 +0530

    refactor: added demo product

---
+++ src/screens/utils.js
@@ -0,0 +1,18 @@
+import "../types";
+
+/**
+ * @type {Product}
+ */
+export const DemoProduct = {
+  id: 0,
+  name: "No product",
+  productImage: "/placeholder.jpg",
+  cost_price: 0,
+  selling_price: 0,
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  allergen_info:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  cooking_instruction:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
