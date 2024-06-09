a74822692865831a24d6b7bd2c16bb7bf0ee82d4
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 12:45:10 +0530

    refactor: added demo data and jsdoc type for Product

---
+++ src/demo-data.js
@@ -0,0 +1,117 @@
+import "./types";
+
+/**
+ * @type {Product[]}
+ */
+export const DemoData = [
+  {
+    name: "Masala Chai Tea Bags",
+    id: "1",
+    description: "Rich Indian tea with a blend of aromatic spices",
+    allergen_info: "Contains no known allergens",
+    cooking_instruction: "Steep in hot water for 4 minutes",
+    cost_price: 50,
+    selling_price: 75,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1688562159ABCChai_Masaldeta.jpg",
+  },
+  {
+    name: "Red Rice",
+    id: "2",
+    description: "Premium red rice",
+    allergen_info: "Gluten-free",
+    cooking_instruction: "Cook with 2 cups of water per cup of rice",
+    cost_price: 80,
+    selling_price: 120,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1708695300-tempName.jpg",
+  },
+  {
+    name: "Paneer Butter Masala",
+    id: "3",
+    description: "Ready-to-eat paneer in a rich tomato sauce",
+    allergen_info: "Contains dairy",
+    cooking_instruction: "Heat for 5 minutes and serve",
+    cost_price: 100,
+    selling_price: 150,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1692423942MenkaPaneer_Butdeta.jpg",
+  },
+  {
+    name: "Mango Pickle",
+    id: "4",
+    description: "Traditional Indian mango pickle with spicy herbs",
+    allergen_info: "May contain mustard",
+    cooking_instruction: "Serve as a condiment",
+    cost_price: 40,
+    selling_price: 60,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1701840858LakshMavinakayideta.jpg",
+  },
+  {
+    name: "Multigrain Atta",
+    id: "5",
+    description: "Healthy blend of multiple grains for roti",
+    allergen_info: "Contains gluten",
+    cooking_instruction: "Mix with water to make dough",
+    cost_price: 200,
+    selling_price: 250,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1707378310-tempName.jpg",
+  },
+  {
+    name: "Garlic",
+    id: "6",
+    description: "Organic Garlic",
+    allergen_info: "None",
+    cooking_instruction: "Add it to your gravy to enhance the taste",
+    cost_price: 30,
+    selling_price: 45,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1709581299-tempName.jpg",
+  },
+  {
+    name: "Spicy Sev",
+    id: "7",
+    description: "Crispy, spicy gram flour noodles",
+    allergen_info: "May contain peanuts",
+    cooking_instruction: "Ready to eat",
+    cost_price: 15,
+    selling_price: 25,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1694840709HardiThin_Sevdeta.jpg",
+  },
+  {
+    name: "Saffron Kheer Mix",
+    id: "8",
+    description: "Instant kheer mix with real saffron strands",
+    allergen_info: "Contains milk",
+    cooking_instruction: "Boil with milk for 5 minutes",
+    cost_price: 50,
+    selling_price: 80,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1697114660HemaKheer_Premdeta.jpg",
+  },
+  {
+    name: "Dosa Batter",
+    id: "9",
+    description: "Dosa Batter - made from the finest ingredients.",
+    allergen_info: "Free from major allergens",
+    cooking_instruction: "Add 100ml water & mix well.",
+    cost_price: 60,
+    selling_price: 90,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1555476230Raji_Premium_iddeta.jpg",
+  },
+  {
+    name: "Whole Wheat Biscuits",
+    id: "10",
+    description: "Healthy whole wheat biscuits for snacks",
+    allergen_info: "Contains gluten",
+    cooking_instruction: "Ready to eat",
+    cost_price: 20,
+    selling_price: 30,
+    productImage:
+      "https://myjaxy-prod.s3.amazonaws.com/1713250489-tempName.jpg",
+  },
+];
---
+++ src/types.js
@@ -0,0 +1,22 @@
+/**
+ * {
+ *  "name": "Masala Chai Tea Bags",
+ *  "id": "1",
+ *  "description": "Rich Indian tea with a blend of aromatic spices",
+ *  "allergen_info": "Contains no known allergens",
+ *  "cooking_instruction": "Steep in hot water for 4 minutes",
+ *  "cost_price": 50,
+ *  "selling_price": 75,
+ *  "productImage": "https://myjaxy-prod.s3.amazonaws.com/1688562159ABCChai_Masaldeta.jpg"
+ * }
+ *
+ * @typedef {object} Product
+ * @property {string} name
+ * @property {string} id
+ * @property {string} description
+ * @property {number} cost_price
+ * @property {number} selling_price
+ * @property {string} allergen_info
+ * @property {string} cooking_instruction
+ * @property {string} productImage
+ */
