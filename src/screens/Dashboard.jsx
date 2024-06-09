44813623f46c5f7d8146b0c20835f41f481c979c
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 18:12:19 +0530

    feat: added search feature to the dashboard

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,20 +1,49 @@
 import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
 import { Container } from "react-bootstrap";
 import { useFetcher } from "../hooks/useFetcher";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function Dashboard() {
+  const [searchText, setSearchText] = useState("");
+  const [fileteredProducts, setFileteredProducts] = useState([]);
   const { makeRequest, output: products } = useFetcher("get");
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
 
   useEffect(() => {
     makeRequest({ url: "/dashboard" });
   }, [makeRequest]);
+
+  useEffect(() => {
+    const timeID = setTimeout(() => {
+      const regex = new RegExp(searchText, "i");
+      const filetered = [...(products ?? [])].filter(
+        (ele) => ele.id.search(regex) !== -1 || ele.name.search(regex) !== -1,
+      );
+      setFileteredProducts(filetered);
+    }, 500); // Debouncing
+
+    return () => {
+      clearTimeout(timeID);
+    };
+  }, [products, searchText]);
 
   return (
     <Container>
       <h6 className="text-primary text-center font-weight-bold text-lg h4 my-4">
         Dashboard
       </h6>
+      <Form.Group className="mb-3" controlId="product-search">
+        <Form.Control
+          type="text"
+          placeholder="Search products by ID or name..."
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </Form.Group>
       <Table responsive striped bordered hover size="sm">
         <thead>
           <tr>
@@ -24,7 +53,7 @@ export function Dashboard() {
           </tr>
         </thead>
         <tbody>
-          {products?.map(({ id, name, selling_price }) => (
+          {fileteredProducts?.map(({ id, name, selling_price }) => (
             <tr key={id}>
               <td>{id}</td>
               <td>{name}</td>
