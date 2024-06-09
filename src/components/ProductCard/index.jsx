7bed62a1e0bc699a86620e0d163852ad815dff5d
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 12:54:44 +0530

    refactor: include price in the card too

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,12 +4,13 @@ import "../../types";
 /**
  * @param {Product}
  */
-export function ProductCard({ name, productImage }) {
+export function ProductCard({ name, productImage, selling_price }) {
   return (
     <Card>
       <Card.Img variant="top" src={productImage} alt={name} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
+        <Card.Text>{selling_price} &#8377;</Card.Text>
       </Card.Body>
     </Card>
   );
