f16800435ec673cd2a093f2ec6342744831bed39
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 12:51:44 +0530

    refactor: fetch data from api

--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -1,12 +1,28 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { ProductCard } from "../components/ProductCard";
-import { DemoData } from "../demo-data";
+import { useEffect, useState } from "react";
+import "../types";
+import { getRequest } from "../axios";
 
 export function Products() {
+  /**
+   * @type {[Product[], import("react").Dispatch<Product[]>]}
+   */
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getRequest("/products")
+      .then((data) => {
+        const output = data.data;
+        setProducts(output);
+      })
+      .catch(console.log);
+  }, []);
+
   return (
     <Row xs={1} md={2} lg={3} className="g-4 p-4">
-      {DemoData.map((ele) => (
+      {products.map((ele) => (
         <Col key={ele.id}>
           <ProductCard {...ele} />
         </Col>
