45140211a1f3060faa81d5a5eaf77b8b5dbb74e9
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 11:49:31 +0530

    feat: added Header component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
+import { Header } from "./components/Header";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
