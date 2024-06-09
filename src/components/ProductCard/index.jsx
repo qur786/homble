62b6e8dbb92fff3201b3a8bb1a1452b88ba1a6a8
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-09 17:38:52 +0530

    refactor: update Product component to work for both products and product details page

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,17 +1,55 @@
 import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
 import "../../types";
+import { ProductCardSkeleton } from "./ProductCardSkeleton";
 
 /**
- * @param {Product}
+ * @param {Product & { fullDetailes: boolean; loading: boolean; }}
  */
-export function ProductCard({ name, productImage, selling_price, id }) {
-  return (
-    <Card as="a" href={`/products/${id}`} className="text-decoration-none">
+export function ProductCard({
+  name,
+  productImage,
+  selling_price,
+  id,
+  description,
+  allergen_info,
+  cooking_instruction,
+  fullDetailes,
+  loading,
+}) {
+  return loading ? (
+    <ProductCardSkeleton fullDetailes={fullDetailes} />
+  ) : (
+    <Card
+      {...(fullDetailes
+        ? {}
+        : {
+            as: "a",
+            href: `/products/${id}`,
+            className: "text-decoration-none",
+          })}
+    >
       <Card.Img variant="top" src={productImage} alt={name} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>{selling_price} &#8377;</Card.Text>
+        {fullDetailes ? (
+          <Accordion alwaysOpen>
+            <Accordion.Item eventKey="product-description">
+              <Accordion.Header>Product Description</Accordion.Header>
+              <Accordion.Body>{description}</Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="product-allergen">
+              <Accordion.Header>Product Allergen Info</Accordion.Header>
+              <Accordion.Body>{allergen_info}</Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="product-usage">
+              <Accordion.Header>Product Usage</Accordion.Header>
+              <Accordion.Body>{cooking_instruction}</Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        ) : undefined}
       </Card.Body>
+      <Card.Footer>Price: {selling_price} &#8377;</Card.Footer>
     </Card>
   );
 }
